c2e52cfb6ac85371ae70b04898124e280c8a706a
jmcPereira <jpereira.dev@gmail.com>
2015-04-30 15:31:45 +0100

    First commit to YAML_loader package.

---
+++ lib/YAML_loader.pm
@@ -0,0 +1,33 @@
+package yaml_loader;
+use YAML::XS 'LoadFile';
+use Data::Dumper;
+
+
+use 5.010;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+
+sub new {
+	my($class, $yaml_file) = @_;
+	my $yaml = LoadFile($yaml_file) or die "Error";
+	print Dumper($yaml);
+	return bless {titulos =>$yaml},$class;
+}
+
+
+sub get_category {
+	my ($self, $word) = @_;
+	foreach my $category ($self->{titulos}){
+			}
+	}
+
+
+
+
+
+1;
+
